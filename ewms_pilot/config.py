b8e3d686f15fae678bfea26c2ebd3520565adbb2
Ric Evans <ric@evans-jacquez.com>
2023-01-19 09:39:40 -0600

    add mq-related env vars (use as defaults) pt-2

---
+++ ewms_pilot/config.py
@@ -0,0 +1,36 @@
+"""Configuration constants."""
+
+import dataclasses as dc
+from typing import Optional
+
+from wipac_dev_tools import from_environment_as_dataclass
+
+# pylint:disable=invalid-name
+
+#
+# Env var constants: set as constants & typecast
+#
+
+
+@dc.dataclass(frozen=True)
+class EnvConfig:
+    """For storing environment variables, typed."""
+
+    # broker -- assumes one broker is the norm
+    EWMS_PILOT_BROKER_ADDRESS: str = "localhost"
+    EWMS_PILOT_BROKER_AUTH_TOKEN: str = ""
+
+    # misc config
+    EWMS_PILOT_PREFETCH: int = 1  # incoming messages
+
+    # timeouts
+    EWMS_PILOT_TIMEOUT_INCOMING: int = 1 * 60
+    EWMS_PILOT_TIMEOUT_OUTGOING: int = 1 * 60
+    EWMS_PILOT_TIMEOUT_WAIT_FOR_FIRST_MESSAGE: Optional[int] = None
+
+    # logging
+    EWMS_PILOT_LOG: str = "INFO"
+    EWMS_PILOT_LOG_THIRD_PARTY: str = "WARNING"
+
+
+ENV = from_environment_as_dataclass(EnvConfig)
