a5615f85024b425447d1667d03771afa14865b34
Ric Evans <ric@evans-jacquez.com>
2023-04-28 15:44:55 -0500

    Revert "use the message id along with timestamp for task-id"
    This reverts commit c71cf1b0f5bce805d33e95ee91fbed4757ab514b.

--- ewms_pilot/pilot.py
+++ ewms_pilot/pilot.py
@@ -178,7 +178,6 @@ def read_from_subproc(
 async def process_msg_task(
     in_msg: Any,
     cmd: str,
-    msg_id: str,
     subproc_timeout: Optional[int],
     #
     ftype_to_subproc: FileType,
@@ -191,7 +190,7 @@ async def process_msg_task(
     pub: mq.queue.QueuePubResource,
 ) -> Any:
     """Process the message's task in a subprocess using `cmd` & respond."""
-    task_id = f"{msg_id}-{time.time()}"  # in case this message has been here before and/or ids cycled
+    task_id = str(time.time())
 
     # debugging logic
     debug_subdir = None
@@ -377,13 +376,6 @@ async def _ack_nack_finished_tasks(
     )
 
 
-def _message_id_str(msg: Message) -> str:
-    if isinstance(msg.msg_id, bytes):
-        return msg.msg_id.decode()
-    else:
-        return str(msg.msg_id)
-
-
 async def _consume_and_reply(
     cmd: str,
     #
@@ -436,7 +428,6 @@ async def _consume_and_reply(
                     process_msg_task(
                         in_msg.data,
                         cmd,
-                        _message_id_str(in_msg),
                         subproc_timeout,
                         ftype_to_subproc,
                         ftype_from_subproc,
--- tests/test_pilot.py
+++ tests/test_pilot.py
@@ -91,7 +91,10 @@ def assert_debug_dir(
     for path in debug_dir.iterdir():
         assert path.is_dir()
 
-        task_id = path.name
+        # this is an epoch timestamp
+        timestamp = float(path.name)
+        assert (time.time() - 120) < timestamp  # a generous diff
+        assert timestamp < time.time()
 
         # look for in/out files
         for subpath in path.iterdir():
@@ -99,10 +102,10 @@ def assert_debug_dir(
         these_files = list(files)  # copies
         if "in" in these_files:
             these_files.remove("in")
-            these_files.append(f"in-{task_id}{ftype_to_subproc.value}")
+            these_files.append(f"in-{timestamp}{ftype_to_subproc.value}")
         if "out" in these_files:
             these_files.remove("out")
-            these_files.append(f"out-{task_id}{ftype_to_subproc.value}")
+            these_files.append(f"out-{timestamp}{ftype_to_subproc.value}")
         assert sorted(p.name for p in path.iterdir()) == sorted(these_files)
 
 
