45f06bc8fc291f28b299c6fa2b7741ce4bf768a8
Ric Evans <ric@evans-jacquez.com>
2023-09-18 12:06:54 -0500

    move `await asyncio.sleep(0)`

--- ewms_pilot/pilot.py
+++ ewms_pilot/pilot.py
@@ -427,14 +427,13 @@ class Housekeeping:
     def __init__(self) -> None:
         self.prev_rabbitmq_heartbeat = 0.0
 
-    def work(
+    async def work(
         self,
         in_queue: mq.Queue,
         sub: mq.queue.ManualQueueSubResource,
         pub: mq.queue.QueuePubResource,
     ) -> None:
         """Do housekeeping."""
-
         # rabbitmq heartbeats
         # TODO: replace when https://github.com/Observation-Management-Service/MQClient/issues/56
         if in_queue._broker_client.NAME.lower() == "rabbitmq":
@@ -448,7 +447,8 @@ class Housekeeping:
                         LOGGER.info("sending heartbeat to RabbitMQ broker...")
                         raw_q.connection.process_data_events()  # type: ignore[attr-defined, union-attr]
 
-        # TODO -- add other housekeeping
+        # other housekeeping
+        await asyncio.sleep(0)  # hand over control to other async tasks
 
 
 async def _consume_and_reply(
@@ -517,8 +517,7 @@ async def _consume_and_reply(
         while not listener_loop_exit(
             task_errors, msg_waittime_current, msg_waittime_timeout
         ):
-            housekeeper.work(in_queue, sub, pub)
-            await asyncio.sleep(0)  # hand over control to other async tasks
+            await housekeeper.work(in_queue, sub, pub)
             #
             # get messages/tasks
             if len(pending) >= multitasking:
@@ -577,7 +576,7 @@ async def _consume_and_reply(
         if pending:
             LOGGER.debug("Waiting for remaining tasks to finish...")
         while pending:
-            housekeeper.work(in_queue, sub, pub)
+            await housekeeper.work(in_queue, sub, pub)
             # wait on finished task (or timeout)
             pending, task_errors = await _wait_on_tasks_with_ack(
                 sub,
