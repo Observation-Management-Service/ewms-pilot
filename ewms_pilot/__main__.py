c5678cdf5179b1d484a86e0a351af0cc27d72712
Ric Evans <ric@evans-jacquez.com>
2023-09-27 12:22:27 -0500

    misc help text

--- ewms_pilot/__main__.py
+++ ewms_pilot/__main__.py
@@ -12,27 +12,27 @@ from .pilot import consume_and_reply
 
 
 def main() -> None:
-    """Start up EWMS Pilot subprocess to perform an MQ task."""
+    """Start up EWMS Pilot to do tasks, communicate via message passing."""
 
     parser = argparse.ArgumentParser(
-        description="Start up EWMS Pilot subprocess to perform an MQ task",
+        description="Start up EWMS Pilot task to perform an MQ task",
         epilog="",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
         "--cmd",  # alternatively we can go with a condor-like --executable and --arguments
         required=True,
-        help="the command to give the subprocess script",
+        help="the command to give the task script",
     )
     parser.add_argument(
         "--infile-type",
         type=FileType,
-        help="the file type (extension) to use for files written for the pilot's subprocess",
+        help="the file type (extension) of the input file for the pilot's task",
     )
     parser.add_argument(
         "--outfile-type",
         type=FileType,
-        help="the file type (exception) of the file to read from the pilot's subprocess",
+        help="the file type (extension) of the output file from the pilot's task",
     )
     parser.add_argument(
         "--multitasking",
