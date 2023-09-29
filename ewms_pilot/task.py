6da47b33b784c4a81668e73dc0d7aff466dee1e8
Ric Evans <ric@evans-jacquez.com>
2023-09-29 10:27:57 -0500

    Split Off `subproc.py`

--- ewms_pilot/task.py
+++ ewms_pilot/task.py
@@ -1,63 +1,15 @@
 """Single task logic."""
 
 
-import asyncio
-import shlex
 import shutil
-import sys
 from pathlib import Path
-from typing import Any, Callable, Optional, TextIO
+from typing import Any, Callable, Optional
 
 from mqclient.broker_client_interface import Message
 
 from .config import LOGGER
 from .io import FileType
-
-
-def get_last_line(fpath: Path) -> str:
-    """Get the last line of the file."""
-    with fpath.open() as f:
-        line = ""
-        for line in f:
-            pass
-        return line.rstrip()  # remove trailing '\n'
-
-
-class TaskSubprocessError(Exception):
-    """Raised when the subprocess terminates in an error."""
-
-    def __init__(self, return_code: int, stderrfile: Path):
-        super().__init__(
-            f"Subprocess completed with exit code {return_code}: "
-            f"{get_last_line(stderrfile)}"
-        )
-
-
-def mv_or_rm_file(src: Path, dest: Optional[Path]) -> None:
-    """Move the file to `dest` if not None, else rm it.
-
-    No error if file doesn't exist.
-    """
-    if not src.exists():
-        return
-    if dest:
-        # src.rename(dest / src.name)  # mv
-        # NOTE: https://github.com/python/cpython/pull/30650
-        shutil.move(str(src), str(dest / src.name))  # py 3.6 requires strs
-    else:
-        src.unlink()  # rm
-
-
-def _dump_binary_file(fpath: Path, stream: TextIO) -> None:
-    try:
-        with open(fpath, "rb") as file:
-            while True:
-                chunk = file.read(4096)
-                if not chunk:
-                    break
-                stream.buffer.write(chunk)
-    except Exception as e:
-        LOGGER.error(f"Error dumping subprocess output ({stream.name}): {e}")
+from .subproc import run_subproc
 
 
 async def process_msg_task(
@@ -91,40 +43,9 @@ async def process_msg_task(
     cmd = cmd.replace("{{INFILE}}", str(infilepath))
     cmd = cmd.replace("{{OUTFILE}}", str(outfilepath))
 
-    # write message for subproc
+    # run
     file_writer(in_msg.data, infilepath)
-
-    # call & check outputs
-    LOGGER.info(f"Executing: {shlex.split(cmd)}")
-    try:
-        with open(stdoutfile, "wb") as stdoutf, open(stderrfile, "wb") as stderrf:
-            # await to start & prep coroutines
-            proc = await asyncio.create_subprocess_shell(
-                cmd,
-                stdout=stdoutf,
-                stderr=stderrf,
-            )
-            # await to finish
-            await asyncio.wait_for(  # raises TimeoutError
-                proc.wait(),
-                timeout=task_timeout,
-            )
-
-        LOGGER.info(f"Subprocess return code: {proc.returncode}")
-
-        # exception handling (immediately re-handled by 'except' below)
-        if proc.returncode:
-            raise TaskSubprocessError(proc.returncode, stderrfile)
-
-    except Exception as e:
-        LOGGER.error(f"Subprocess failed: {e}")  # log the time
-        raise
-    finally:
-        if dump_task_output:
-            _dump_binary_file(stdoutfile, sys.stdout)
-            _dump_binary_file(stderrfile, sys.stderr)
-
-    # Successful Case: get message and move to debug dir
+    await run_subproc(cmd, task_timeout, stdoutfile, stderrfile, dump_task_output)
     out_data = file_reader(outfilepath)
 
     # send
