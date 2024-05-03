93187585280e6fe3cadb9f5a9ba2652a4ab7d991
ric-evans <emejqz@gmail.com>
2024-05-03 11:40:21 -0500

    update example script

--- examples/do_task.py
+++ examples/do_task.py
@@ -3,26 +3,20 @@
 See https://github.com/Observation-Management-Service/ewms-workflow-management-service/blob/main/examples/request_task.py
 """
 
-
-import argparse
 import asyncio
 import logging
 from pathlib import Path
 
-from ewms_pilot import FileType, consume_and_reply
+from ewms_pilot import consume_and_reply
 
 LOGGER = logging.getLogger(__name__)
 
 
-async def main(
-    queue_incoming: str,
-    queue_outgoing: str,
-    debug_dir: Path,
-) -> None:
+async def main() -> None:
     """Test a normal .txt-based pilot."""
 
     await consume_and_reply(
-        # task is to double the input, one-at-a-time
+        # task is to "double" the input, one-at-a-time
         cmd="""python3 -c "
 import sys
 import time
@@ -34,32 +28,9 @@ time.sleep(5)
 print('printed: ' + output)
 print(output, file=open('{{OUTFILE}}','w'))
 " """,
-        queue_incoming=queue_incoming,
-        queue_outgoing=queue_outgoing,
-        ftype_to_subproc=FileType.TXT,
-        ftype_from_subproc=FileType.TXT,
-        debug_dir=debug_dir,
+        debug_dir=Path("."),
     )
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--queue-incoming",
-        required=True,
-        help="the name of the incoming queue",
-    )
-    parser.add_argument(
-        "--queue-outgoing",
-        required=True,
-        help="the name of the outgoing queue",
-    )
-    args = parser.parse_args()
-
-    asyncio.run(
-        main(
-            args.queue_incoming,
-            args.queue_outgoing,
-            Path("."),
-        )
-    )
+    asyncio.run(main())
