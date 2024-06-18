b19b21c57952ad8736e86bf103f1f058c4226b42
ric-evans <emejqz@gmail.com>
2024-06-18 16:46:01 -0400

    force extension to lower case

--- ewms_pilot/tasks/io.py
+++ ewms_pilot/tasks/io.py
@@ -5,7 +5,7 @@ class FileExtension:
     """Really, this just strips the dot off the file extension string."""
 
     def __init__(self, extension: str):
-        self.val = extension.lstrip(".")
+        self.val = extension.lstrip(".").lower()
 
     def __str__(self) -> str:
         return self.val
