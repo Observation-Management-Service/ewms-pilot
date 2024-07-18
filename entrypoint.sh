91cc04b20bdefa0ebc2c459015bd7ec9fba7b3ed
ric-evans <emejqz@gmail.com>
2024-07-18 14:47:22 -0400

    (debug)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "entrypoint: activating docker daemon"
 dockerd > /var/log/dockerd.log 2>&1 &
+sleep 15
 echo "entrypoint: activating venv"
 source /app/entrypoint_venv/bin/activate
 echo "entrypoint: executing command: $@"
