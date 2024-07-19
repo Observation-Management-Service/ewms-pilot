62f8fb924bf3826a8a2765bd547ac806fd5e32f3
ric-evans <emejqz@gmail.com>
2024-07-19 12:44:54 -0400

    remove `docker ps` from `entrypoint.sh`

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 echo "entrypoint: activating docker daemon..."
 dockerd > /var/log/dockerd.log 2>&1 &
 sleep 1
-docker ps
 echo "entrypoint: activating venv"
 source /app/entrypoint_venv/bin/activate
 echo "entrypoint: executing command: $@"
