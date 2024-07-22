a94bcd3978ad704d0c35b46b72ce55111a7efe52
ric-evans <emejqz@gmail.com>
2024-07-22 18:03:03 -0400

    (temp: pre-pull in entrypoint.sh)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 echo "entrypoint: activating docker daemon..."
 dockerd > /var/log/dockerd.log 2>&1 &
 sleep 1
+# TODO: remove this and put in ci cmd
+docker pull python:alpine
 echo "entrypoint: activating venv"
 source /app/entrypoint_venv/bin/activate
 echo "entrypoint: executing command: $@"
