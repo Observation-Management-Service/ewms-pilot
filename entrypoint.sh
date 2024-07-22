15f6f256b1f67b9b8aced8ba00a283267cb24281
ric-evans <emejqz@gmail.com>
2024-07-22 18:14:27 -0400

    do `docker save` & `docker load` for caching

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ echo "entrypoint: activating docker daemon..."
 dockerd > /var/log/dockerd.log 2>&1 &
 sleep 1
 # TODO: remove this and put in ci cmd
-docker pull python:alpine
+docker load -i python-alpine.tar
 echo "entrypoint: activating venv"
 source /app/entrypoint_venv/bin/activate
 echo "entrypoint: executing command: $@"
