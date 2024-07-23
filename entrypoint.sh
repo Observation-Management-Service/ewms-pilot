8913c25d3e6ddf3fb5860ca303b5121acca7a9f1
ric-evans <emejqz@gmail.com>
2024-07-23 09:56:14 -0400

    make it okay if docker-in-docker setup fails in `entrypoint.sh`

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "entrypoint: activating docker daemon..."
-dockerd > /var/log/dockerd.log 2>&1 &
+dockerd > /var/log/dockerd.log 2>&1 || echo "WARNING: docker-in-docker setup failed (error suppressed)" &
 sleep 1
 echo "entrypoint: activating venv"
 source /app/entrypoint_venv/bin/activate
