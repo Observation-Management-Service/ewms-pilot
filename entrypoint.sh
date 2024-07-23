f80b0c3c09c30486e39eafdeacdf3cb683519c88
ric-evans <emejqz@gmail.com>
2024-07-23 10:12:44 -0400

    add entrypoint terminal message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+echo "****************************************************************************************"
+echo "****************************************************************************************"
+echo "**                                                                                    **"
+echo "** Entering the Pilot Environment for the Event Workflow Management System (EWMS)     **"
+echo "**                                                                                    **"
+echo "** -> Source: https://github.com/Observation-Management-Service/ewms-pilot            **"
+echo "**                                                                                    **"
+echo "****************************************************************************************"
+echo "****************************************************************************************"
+
 echo "entrypoint: activating docker daemon..."
 dockerd > /var/log/dockerd.log 2>&1 || echo "WARNING: docker-in-docker setup failed (error suppressed)" &
 sleep 1
