4acd087a302d0aee07810dfb52b63e325be2123e
ric-evans <emejqz@gmail.com>
2024-08-05 16:50:29 -0400

    add `DONE_PHRASE`

--- tests/wait-on-pids.sh
+++ tests/wait-on-pids.sh
@@ -7,11 +7,12 @@ set -ex
 #
 ########################################################################
 
-if [ -z "$1" ]; then
-    echo "Usage: wait-on-pids.sh PIDS_LIST"
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: wait-on-pids.sh PIDS_LIST DONE_PHRASE"
     exit 1
 else
     PIDS_LIST="$1"
+    DONE_PHRASE="$2"
 fi
 
 
@@ -19,17 +20,15 @@ echo "--------------------------------------------------------------"
 echo "will wait on pids: $PIDS_LIST..."
 
 
-# wait for tests to finish
 # https://stackoverflow.com/a/32604828/13156561
-sleep 3  # short sleep to help logs
 for pid in PIDS_LIST; do
     date --rfc-3339=seconds
     echo "waiting for $pid..."
     if ! wait -n $pid; then
-        echo "ERROR: test(s) failed"
+        echo "ERROR: pid $pid exited with non-zero code"
         sleep 5  # may need to wait for output files to be written
         kill $PIDS_LIST 2>/dev/null
         exit 1
     fi
-    echo "-> PASSED"
+    echo "-> $DONE_PHRASE"
 done
