a7ca17c6d69de3f2fad049dc0a8590ecc6909dfb
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2024-08-08 17:10:58 -0400

    Remove `--writable-tmpfs` from Task Container (#88)

--- ewms_pilot/utils/runner.py
+++ ewms_pilot/utils/runner.py
@@ -202,7 +202,7 @@ class ContainerRunner:
             case "apptainer":
                 cmd = (
                     f"apptainer run "
-                    f"--containall --writable-tmpfs --no-eval "  # gets us close to docker functionality
+                    f"--containall --no-eval "  # gets us close to docker functionality
                     f"{mount_bindings} "
                     f"{env_options} "
                     f"{self.image} {inst_args}"
